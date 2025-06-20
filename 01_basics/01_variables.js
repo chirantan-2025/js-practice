90be297a09b59ab98ea22d9a9b76da40895e11a4
chirantan-2025 <chirantanbramhachari560@gmail.com>
2025-06-20 17:21:08 +0000

    types of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "chirantan@gamil.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let account;
+
+// accountId =2
+
+accountEmail="abc@gamil.com"
+accountPassword="23455"
+accountCity = "kolkata"
+
+// console.log(accountId);
+/* Prefer not to use var because of issue in block scope and functional scope */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,account])
