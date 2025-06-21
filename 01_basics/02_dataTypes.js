95898949deb6357f5d8138124c1def5e0f96e4cf
chirantan-2025 <chirantanbramhachari560@gmail.com>
2025-06-21 15:33:38 +0000

    devcontainer updated

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("chirantan");
+
+let name = "chirantan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assined
+// symbol => to find uniqueness in React components
+
+
+// object
+console.log(typeof "chirantan")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
+
+
